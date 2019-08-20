880c17c84e3908f84eb7cd92a097e4711cc95601
Radosław Lejman <42996695+RadekeL@users.noreply.github.com>
2019-08-20 10:21:49 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,76 +1,69 @@
 
+const hourBox = document.querySelector('.clock-box__hour');
+const minuteBox = document.querySelector('.clock-box__minute');
+const secondBox = document.querySelector('.clock-box__second');
 
-function clock (){
-    const data = new Date();
-    const hourBox = document.querySelector('.clock-box__hour');
-    const minuteBox = document.querySelector('.clock-box__minute');
-    const secondBox = document.querySelector('.clock-box__second');
-    
-    let counter = 0; // check first round in h/min/sec
-    
-    const currentHour = data.getHours() >=12 ? data.getHours() - 12 : data.getHours;
-    const currentMin = data.getMinutes();
-    const currentSec = data.getSeconds();
-    
-    // Uploading initial clock data
-    (function(){
-        hourBox.textContent = currentHour.toString().padStart(2,'0');
-        minuteBox.textContent = currentMin.toString().padStart(2,'0');
-        secondBox.textContent = currentSec.toString().padStart(2,'0');
-    })();
-    
-    function* hours(hour){
-        while(true){
-            yield hour++
-            if(hour === 12){
-                hour = 0;
-            }
-        }
-    }
-    
-    function* minute(min){
-        const firstRound = 2;
-        while(true){
-            yield min++;
-            if(min===60){
-                if(counter===firstRound){// call double iteratorHour
-                    iteratorHour.next().value;
-                    counter++;
-                }
-                min = 0;
-               hourBox.textContent = `${iteratorHour.next().value}`.padStart(2,'0')
-            }
-        }
-    }
-    
-    function* second(sec){
-        const firstRound = 1;
-        while(true){
-            yield sec++;
-            if(sec===60){
-                if(counter===firstRound){// call double iteratorMin
-                    iteratorMin.next().value;
-                    counter++;
-                }
-                sec = 0;
-                minuteBox.textContent = `${iteratorMin.next().value}`.padStart(2,'0')
-            }
-        }
-    }
-    
-    const iteratorSec = second(data.getSeconds());
-    const iteratorMin = minute(data.getMinutes());
-    const iteratorHour = hours(data.getHours());
-    
-    
-    setInterval(()=>{
-        if(counter===0){// call double iteratorSec  
-            iteratorSec.next().value
-            counter++;      
-        } 
-        secondBox.textContent= `${iteratorSec.next().value}`.padStart(2,'0')
-    },1000)
-    }
-    
-    clock();
+const data = new Date();
 
+let counter = 0; // check first round in h/min/sec
+
+// Uploading initial clock data
+(function(){
+    hourBox.textContent = data.getHours().toString().padStart(2,'0');
+    minuteBox.textContent = data.getMinutes().toString().padStart(2,'0');
+    secondBox.textContent = data.getSeconds().toString().padStart(2,'0');
+
+})();
+
+function* hours(hour){
+    while(true){
+        yield hour++
+        if(hour === 12){
+            hour = 0;
+        }
+    }
+}
+
+function* minute(min){
+    const firstRound = 2;
+    while(true){
+        yield min++;
+        if(min===60){
+            if(counter===firstRound){// call double iteratorHour
+                iteratorHour.next().value;
+                counter++;
+            }
+            min = 0;
+           hourBox.textContent = `${iteratorHour.next().value}`.padStart(2,'0')
+        }
+    }
+}
+
+function* second(sec){
+    const firstRound = 1;
+    while(true){
+        yield sec++;
+        if(sec===60){
+            if(counter===firstRound){// call double iteratorMin
+                iteratorMin.next().value;
+                counter++;
+            }
+            sec = 0;
+            minuteBox.textContent = `${iteratorMin.next().value}`.padStart(2,'0')
+        }
+    }
+}
+
+const iteratorSec = second(data.getSeconds());
+const iteratorMin = minute(data.getMinutes());
+const iteratorHour = hours(data.getHours());
+
+
+
+const writeSeconds = setInterval(()=>{
+    if(counter===0){// call double iteratorSec  
+        iteratorSec.next().value
+        counter++;      
+    } 
+    secondBox.textContent= `${iteratorSec.next().value}`.padStart(2,'0')
+},1000)
